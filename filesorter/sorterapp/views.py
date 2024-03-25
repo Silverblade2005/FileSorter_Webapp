7c4aa637472ca8e17fc492f0fcac09d2cdd2f7d3
Silverblade2005 <63549786+Silverblade2005@users.noreply.github.com>
2024-03-25 17:57:19 +0200

    Completed feature to add extentions

--- filesorter/sorterapp/views.py
+++ filesorter/sorterapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import UploadedFile
+from .models import UploadedFile, AudioExtention, VideoExtention,DocumentExtention,ImageExtention
 from .sorter_module import run_sorter
 import os
 
@@ -32,4 +32,32 @@ def home(request):
     return render(request, 'index.html', {})
 
 def settings(request):
-    return render(request, 'settings.html', {})
+
+    if request.method == "POST":
+        is_video = request.POST.get('is_video')
+        is_audio = request.POST.get('is_audio')
+        is_document = request.POST.get('is_document')
+        is_image = request.POST.get('is_image')
+        extention_field = request.POST["extention_field"]
+
+        if is_video != None:
+            new_ext = VideoExtention(extention=extention_field)
+            new_ext.save()
+        if is_audio != None:
+            new_ext = AudioExtention(extention=extention_field)
+            new_ext.save()
+        if is_document != None:
+            new_ext = DocumentExtention(extention=extention_field)
+            new_ext.save()
+        if is_image != None:
+            new_ext = ImageExtention(extention=extention_field)
+            new_ext.save()
+        
+        
+
+    return render(request, 'settings.html', {
+        "AudioExtentions": AudioExtention.objects.all(),
+        "VideosExtentions": VideoExtention.objects.all(),
+        "ImagesExtentions": ImageExtention.objects.all(),
+        "DocumentExtentions": DocumentExtention.objects.all(),
+    })
